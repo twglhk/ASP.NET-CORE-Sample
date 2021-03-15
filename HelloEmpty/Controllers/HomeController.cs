7244f67d340ee514c10f76118133048a61960423
John <twglhk@gmail.com>
2021-03-15 20:19:09 +0900

    Add Empty MVC

---
+++ HelloEmpty/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloEmpty.Models;
+
+namespace HelloEmpty.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            HelloMessage msg = new HelloMessage()
+            {
+                Message = "Welcome to ASP.NET MVC!"
+            };
+
+            ViewBag.Noti = "Input message and click summit";
+
+            return View();
+        }
+    }
+}
