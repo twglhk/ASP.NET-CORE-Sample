239aa76559d35de8640f15dcb024b9a291260797
John <twglhk@gmail.com>
2021-03-17 10:31:10 +0900

    Add Razor page sample

--- HelloEmpty/Startup.cs
+++ HelloEmpty/Startup.cs
@@ -16,7 +16,7 @@ namespace HelloEmpty
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,9 +31,7 @@ namespace HelloEmpty
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
