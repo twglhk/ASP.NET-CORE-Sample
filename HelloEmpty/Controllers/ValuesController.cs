846e19f50c9ae0c71af8b3ef5875dc2da2ddfbef
John <twglhk@gmail.com>
2021-03-18 09:22:22 +0900

    Add WebAPI to empty project

---
+++ HelloEmpty/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloEmpty.Models;
+
+namespace HelloEmpty.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public List<HelloMessage> Get()
+        {
+            List<HelloMessage> messages = new List<HelloMessage>();
+            messages.Add(new HelloMessage() { Message = "Hello Web API1" });
+            messages.Add(new HelloMessage() { Message = "Hello Web API2" });
+            messages.Add(new HelloMessage() { Message = "Hello Web API3" });
+
+            return messages;
+        }
+    }
+}
--- HelloEmpty/Startup.cs
+++ HelloEmpty/Startup.cs
@@ -16,7 +16,7 @@ namespace HelloEmpty
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,7 +31,7 @@ namespace HelloEmpty
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
