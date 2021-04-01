bf5ab09d64051f64709a749c08943ed3fdd04ed0
John <twglhk@gmail.com>
2021-04-01 10:22:10 +0900

    add ranking table with Entity Framework

---
+++ RankingApp/RankingApp/Data/Models/GameResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RankingApp.Data.Models
+{
+    // ORM을 사용하지 않는다면 테이블에 직접 column을 만들어야함.
+    public class GameResult
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public int Score { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
