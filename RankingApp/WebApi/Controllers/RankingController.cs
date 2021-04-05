e8709248b7f74941ad9f5a635dc983f451251a71
John <twglhk@gmail.com>
2021-04-05 12:11:29 +0900

    add : web api controller

---
+++ RankingApp/WebApi/Controllers/RankingController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SharedData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Data;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RankingController : ControllerBase
+    {
+        ApplicationDbContext _context;
+
+        public RankingController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Create
+
+        // Read
+        [HttpGet]
+        public List<GameResult> GetGameResults()
+        {
+            List<GameResult> results = _context.GameResults
+                .OrderByDescending(item => item.Score)
+                .ToList();
+
+            return results;
+        }
+
+        [HttpGet("{id}")]   // ex) GET api/ranking/1
+        public GameResult GetGameResults(int id)
+        {
+            GameResult results = _context.GameResults
+                .OrderByDescending(item => item.Id == id)
+                .FirstOrDefault();
+
+            return results;
+        }
+
+
+        // Update
+
+
+        // Delete
+
+    }
+}
